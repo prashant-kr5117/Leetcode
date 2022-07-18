ab89398a2ef4b9bd15d52685f386a7423dc141b9
Prashant Kumar <67841132+prashant-kr5117@users.noreply.github.com>
2022-07-18 13:18:53 +0530

    Time: 13 ms (5.54%), Space: 9 MB (58.99%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -5,8 +5,8 @@ public:
        int ma =  *max_element(can.begin(), can.end());
      vector<bool> res;
         for(int i=0;i<can.size();i++){
-            if(can[i]+eC >= ma) res.push_back(true);
-            else res.push_back(false);
+            can[i]+eC >= ma ? res.push_back(true):res.push_back(false);
+             
         }
         return res;
     }
