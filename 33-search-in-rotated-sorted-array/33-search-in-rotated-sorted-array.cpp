8687dd66adb735e490d0c6ba5873a9b004c3b217
Prashant Kumar <67841132+prashant-kr5117@users.noreply.github.com>
2022-08-01 01:56:41 +0530

    Time: 6 ms (58.01%), Space: 11.1 MB (29.16%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // vector<int> res(target.begin(),target.end());
         
-//         target=target%size(nums);
-        
-//         for(int i=target+1;i<nums.size();i++){
-//             res.push_back(nums[i]);
-//         }
-        
-//         for(int i=0;i<=target;i++){
-//             res.push_back(nums[i]);
-//         }
-        
-        auto i= find(nums.begin(),nums.end(),target);
-        if(i==nums.end()) return -1;
-         return i-nums.begin();
-        
+        // auto i= find(nums.begin(),nums.end(),target);
+        // if(i==nums.end()) return -1;
+        //  return i-nums.begin();
+    int l=0,h=nums.size()-1;
+        while(l<=h){
+            int mid=(l+h)>>1;
+        if(nums[mid]==target) return mid;
+        if(nums[l]<=nums[mid]){
+            if(nums[l]<=target && nums[mid]>=target) h=mid-1;
+            else l=mid+1;
+        }
+         else{
+                if(nums[mid]<=target && nums[h]>=target) l=mid+1;
+                 else h=mid-1;
+            }
+            
+    }
+        return -1;
     }
 };
