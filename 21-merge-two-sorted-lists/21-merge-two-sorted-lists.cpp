ba0de207c44115197b7b5b02912219caac1e61fe
Prashant Kumar <67841132+prashant-kr5117@users.noreply.github.com>
2022-09-25 22:34:31 +0530

    Time: 6 ms (90.94%), Space: 14.8 MB (42.58%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,50 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
         
-        ListNode* ptr=list1;
-        if(list1->val>list2->val){
-            ptr =list2;
-            list2=list2->next;
-        }
-        else list1=list1->next;
+        ListNode* ans = new ListNode(-1);
+        ListNode* tail = ans;
         
-        ListNode * cur=ptr;
-        
-        while(list1 && list2){
-            if(list1->val<list2->val){
-                cur->next=list1;
-                list1=list1->next;
+        while(l1 && l2){
+            if(l1->val<l2->val){
+                tail->next = l1;
+                l1=l1->next;    
             }
             else{
-                cur->next=list2;
-                list2=list2->next;
+                tail->next = l2;
+                l2=l2->next;
             }
-            cur=cur->next;
+            tail=tail->next;
         }
+        if(!l1) tail->next=l2;
+        else tail->next = l1;
         
-        while(list1){
-             cur->next=list1;
-                list1=list1->next;
-            cur=cur->next;
-        }
-        while(list2){
-             cur->next=list2;
-                list2=list2->next;
-             cur=cur->next;
-        }
-        return ptr;
+        return ans->next;
     }
 };
